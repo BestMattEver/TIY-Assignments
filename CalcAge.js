a815e5596cce008ee1c21338cfc4c77da5395a01
BestMatEver <mattkun@gmail.com>
2016-02-10 10:38:36 -0500

    added the object recipie exercise to calcAge.js

--- CalcAge.js
+++ CalcAge.js
@@ -1,3 +1,4 @@
+//--------This calculates an age based on the current year and a year of birth---
 function calculateAge (curYear, birthYear)
 {
   var posAge = curYear-birthYear;
@@ -7,3 +8,16 @@ function calculateAge (curYear, birthYear)
 calculateAge(2016, 1986);
 calculateAge(2015, 1564); //last year shakespeare was...
 calculateAge(1963, 1917); //jfk was 46 when he died.
+
+//----------Object practice--------
+console.log("");
+
+var GoodFood = {
+  title: "Shredded BBQ Chicken",
+  servings: "10",
+  ingredients: ["Chicken Breasts","BBQ Sauce", "Slow Cooker"]
+}
+
+console.log(GoodFood.title);
+console.log(GoodFood.servings);
+console.log(GoodFood.ingredients);
